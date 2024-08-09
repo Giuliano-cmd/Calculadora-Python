2dd7fff60fd5ca4c413f26b489ff7aa9336b71ec
Giuliano <giulianogarfagnoli@gmail.com>
2024-08-09 20:10:15 -0300

    Se cambio todo el programa principal (while), se puede sumar y restar sin usar espacios

--- calculadoraV2.py
+++ calculadoraV2.py
@@ -7,17 +7,33 @@ import time
 os.system ("cls")
 print("---------------------")
 calculo = str(input(""))
+calculo =  calculo + "/"
 # print("---------------------\n" + pantalla + "\n---------------------")
 i = 0
 resultado = 0
 numeroTemp = ""
+primerNum = False
+opeActual = ""
 #print (var)
 while i < len(calculo):
     if calculo[i].isdecimal():
         numeroTemp = numeroTemp + calculo[i]
         print(numeroTemp)
-    else:
+    if primerNum == False and calculo[i].isdecimal() == False:
         resultado = resultado + int(numeroTemp)
         numeroTemp = ""
+        primerNum = True
+        opeActual = calculo[i]
+    elif primerNum == True and calculo[i].isdecimal() == False:
+        if opeActual == "+":
+            resultado = resultado + int(numeroTemp)
+            numeroTemp = ""
+            opeActual = calculo[i]
+        elif opeActual == "-":
+            resultado = resultado - int(numeroTemp)
+            numeroTemp = ""
+            opeActual = calculo[i]
+        else:
+            pass
     i = i+1
 print("Resultado: " + str(resultado))
